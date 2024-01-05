f0980c797363d4d244c8d50d9338be53fc76abfd
SayaleeDamle <sayalee.damle@onepointltd.com>
2024-01-05 17:57:21 +0530

    chainlit application

--- knowledge_graph_chainlit/frontend/main.py
+++ knowledge_graph_chainlit/frontend/main.py
@@ -6,7 +6,7 @@ from knowledge_graph.configuration.log_factory import logger
 from knowledge_graph.configuration.config import cfg
 import knowledge_graph.backend.qna_service as qna
 import knowledge_graph.services.vector_db as v_db
-#import knowledge_graph.frontend.input_text as chk_inp
+import knowledge_graph.frontend.text_finder as chk_inp
 
 
 from knowledge_graph_chainlit.configuration.model import ResponseTags
@@ -21,27 +21,29 @@ async def ask_user_msg(question):
         ans = await cl.AskUserMessage(
             content=f"{question}", timeout=cfg.ui_timeout, raise_on_timeout= True
         ).send()
+    print(ans)
     return ans
 
 @cl.on_chat_start
 async def on_chat_start():
 
     await cl.Message(content="Create knowledge Graphs for the data you give").send()
-    inp_text = await ask_user_msg("Give the input text")
-    #if not await chk_inp.check_if_text_exists(inp_text):
-    table = await ontology.return_ontology(inp_text)
-    await cl.Message(content=table).send()
-    ontology_relations, ontology_terms = await ontology.extract_ontology(table)
-    G = await create_g.create_network(list(ontology_relations))
-    path_description = await create_g.create_subgraph(G)
-    await cl.Message(content=f"The summary derived from graphs is stored here: {path_description}").send()
-    #await chk_inp.put_into_directory(inp_text)
-    db_text = await v_db.create_embeddings_text(inp_text)
-    db_summary = await v_db.create_embeddings_summary(Path(path_description))
+    input_text = await ask_user_msg("Give the input text")
+    content = input_text['output']
+    if not await chk_inp.check_if_text_exists(content):
+        table = await ontology.return_ontology(content)
+        await cl.Message(content=table).send()
+        ontology_relations, ontology_terms = await ontology.extract_ontology(table)
+        G = await create_g.create_network(list(ontology_relations))
+        path_description = await create_g.create_subgraph(G)
+        await cl.Message(content=f"The summary derived from graphs is stored here: {path_description}").send()
+        await chk_inp.put_into_directory(content)
+        db_text = await v_db.create_embeddings_text(content)
+        db_summary = await v_db.create_embeddings_summary(Path(path_description))
 
     while True:
         ques = await ask_user_msg("Please ask any question realted to the topic if you want to")
-        ans = await qna.return_answer(ques)
+        ans = await qna.return_answer(ques['output'])
         await cl.Message(content=ans).send()
 
 
